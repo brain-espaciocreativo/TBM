c27efe7247ad422969f350f2d32bfa6008b27367
YoelFerreyra <89047058+YoelFerreyra@users.noreply.github.com>
2022-08-17 00:56:18 -0300

    back funcionando

--- server/src/db.js
+++ server/src/db.js
@@ -3,9 +3,9 @@ const { Sequelize } = require("sequelize");
 
 // console.log(process.env)
 
-const database = ''
-const username = ''
-const password = ''
+const database = 'pokemon'
+const username = 'postgres'
+const password = 'Yoel1234'
 
 const sequelize = new Sequelize(database, username, password, {
   host: 'localhost',
