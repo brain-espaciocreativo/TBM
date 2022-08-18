ab69bc2db79dcea5b6cc010b3ceeb93c50469909
brayann verona <brayann.fave@gmail.com>
2022-08-18 10:41:26 -0300

    se agregró los modelos y las relaciones

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,8 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
+// const User = require('../models/User');
+// const Work = require('../models/Work');
+
 
 
 const database = process.env.DB || 'TBM';
@@ -11,7 +14,8 @@ const sequelize = new Sequelize(database, username, password, {
   host,
   dialect: 'postgres'
 });
-
+// User.belongsToMany(Work, { through: 'user_work' });
+// Work.belongsToMany(User, { through: 'user_work' });
 module.exports = {
   conn: sequelize,
 
