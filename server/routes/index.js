e7ee381c67797ba3f8948f338c9d95183513e56c
brayann verona <brayann.fave@gmail.com>
2022-08-29 16:31:18 -0300

    modificando commits

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,15 +2,17 @@ const { Router } = require('express');
 const router = Router();
 
 const user = require('./user');
-const categories = require('./categories')
-const news = require('./news')
-const work = require('./work')
-const progress = require('./progress')
+const categories = require('./categories');
+const news = require('./news');
+const work = require('./work');
+const progress = require('./progress');
+const authSesion = require('./authSession');
 
 router.use('/user', user);
 router.use('/categories', categories);
 router.use('/news', news);
-router.use('/work', work)
-router.use('/progress', progress)
+router.use('/work', work);
+router.use('/progress', progress);
+router.use('/auth', authSesion)
 
 module.exports = router
