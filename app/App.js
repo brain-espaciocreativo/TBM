e6e66666d07d1588659ddd6983ef44241427ba8b
Brayann Farfán <77312455+BrayannFarfan@users.noreply.github.com>
2022-09-14 10:10:24 -0300

    creacion de loader para inicio de sesion y creacion de componente Toast reutilizable

--- app/App.js
+++ app/App.js
@@ -4,11 +4,13 @@ import { Provider as PaperProvider } from 'react-native-paper';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import Navigation from "./Navigation";
+import Toast from "./components/Toast";
 
 export default function App() {
   return (
     <Provider store={store}>
       <PaperProvider>
+        <Toast />
         <Navigation/>
       </PaperProvider>
     </Provider>
