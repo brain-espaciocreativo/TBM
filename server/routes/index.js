d8c334a9fb27f0c71070dfbc2a25f6382c6f00b6
Eric Yoel Ferreyra <89047058+YoelFerreyra@users.noreply.github.com>
2022-09-26 18:59:34 -0300

    Creacion de ruta para enviar emails (#29)

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,12 +7,14 @@ const news = require('./news');
 const work = require('./work');
 const progress = require('./progress');
 const authSesion = require('./authSession');
+const sendMail = require('./sendMail');
 
 router.use('/user', user);
 router.use('/categories', categories);
 router.use('/news', news);
 router.use('/work', work);
 router.use('/progress', progress);
-router.use('/auth', authSesion)
+router.use('/auth', authSesion);
+router.use('/email', sendMail);
 
 module.exports = router
