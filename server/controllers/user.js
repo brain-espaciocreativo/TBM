66df7a8526bfa574853b0200ce172d758ca41a21
Brayann Farfán <77312455+BrayannFarfan@users.noreply.github.com>
2022-09-26 19:09:53 -0300

    envio de email para actualizacion de contraseña, actualizacion de contraseña

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,4 +1,6 @@
 const {Users, Works, News, Progress, Categories} = require('../models');
+const nodemailer =  require('nodemailer');
+const jwt = require('jsonwebtoken');
 const hash = require('object-hash');
 
 const getAllUser = async(req, res)=>{
@@ -74,11 +76,121 @@ const deleteOneUser = async(req, res)=>{
         throw Error(res.status(500).send({status: 500, data: "No se eliminó correctamente"}));
     }
 }
+ const forgotPass = async(req, res) =>{
+     if(req.body.email === ""){
+         res.status(400).send({message:"el email es requerido"})
+     }
+     try {
+         const user = await Users.findOne({
+             where:{
+                 email: req.body.email
+             }
+         })
+         if(!user){
+             return res.status(403).send({message:"no existe ese email"})
+         }
+
+         const token = jwt.sign({id: user.id}, 'ejemplodeprueba', {expiresIn:"5m"})
+         const setusertoken = await user.update({
+            token,
+         })
+
+         const transporter = nodemailer.createTransport({
+             service:'gmail',
+             auth:{
+                 user:'brayann.fave@gmail.com',
+                 pass:'whpzxxvkzejrdicb',
+             }
+         })
+
+        if(setusertoken){
+            const mailOptions = {
+                from: 'brayann.fave@gmail.com',
+                to:`${user.email}`,
+                subject: 'enlace para recuperar su cuenta',
+                text: `http://localhost:3000/forgotpassword/${token}`
+            };
+           console.log(mailOptions);
+            transporter.sendMail(mailOptions, (err, response) =>{
+                if(err){
+                    res.status(200).json('No se envio el email')
+                }else{
+                    res.status(200).json('el email para la recuperacion ha sido enviado')
+                }
+            })
+        }
+     } catch (error) {
+         res.status(500).send({
+             message:'ha ocurrido un error',
+             error
+         })
+     }
+ }
+const reset = async(req, res) =>{
+    const { token }= req.params
+
+    try {
+        const validuser = await Users.findOne({
+            token
+        })
+        const verifytoken = jwt.verify(token, 'ejemplodeprueba',{expiresIn:"5m"})
+
+        if (validuser && verifytoken.id){
+            res.status(201).json({status:201, validuser})
+        }else{
+            res.status(401).json({status:401, message:"link no existe"})
+        }
+    } catch (error) {
+    }
+
+  
+}
+  const updatePass = async(req, res) =>{
+
+    const { token } = req.params;
+           if(req.body.password === ""){
+               res.status(400).send({
+                   message: 'ingrese password'
+               });
+               return;
+           }
+    try {
+        const pass = req.body.password
+        const verifytoken = jwt.verify(token, 'ejemplodeprueba');
+        const validuser = await Users.findOne({
+            where:{
+                id: verifytoken.id
+            }
+        })
+        if(validuser && validuser.id){
+            const newpassword = hash.MD5(pass)
+            const resetPassword = await Users.update({password:newpassword},{
+                where: {
+                    id:validuser.id
+                }
+            })
+            res.status(201).send({
+                status:(201),
+                message:"se cambio la contraseña exitosamente"
+            })
+            console.log('toda info updateada',resetPassword);
+        }
+    } catch (error) {
+        res.status(500).send({
+            message: 'este error',
+            error
+        })
+    }
+  }
+
 
 module.exports = {
     getAllUser,
     getOneUser,
     createOneUser,
     updateOneUser,
-    deleteOneUser
+    deleteOneUser,
+    forgotPass,
+    updatePass,
+    reset
 }
--- server/routes/user.js
+++ server/routes/user.js
@@ -3,9 +3,13 @@ const router = Router();
 const userController = require('../controllers/user');
 
 router.get('/', userController.getAllUser);
-router.get('/:id', userController.getOneUser)
+router.get('/:id', userController.getOneUser);
 router.post('/', userController.createOneUser);
 router.put('/:id', userController.updateOneUser);
 router.delete("/:id", userController.deleteOneUser);
+
+router.post('/sendpasswordlink', userController.forgotPass)
+router.get('/reset/:id/:token', userController.reset)
+router.put('/updatepassword/:id/:token', userController.updatePass)
 
 module.exports = router;
