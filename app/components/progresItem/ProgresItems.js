36c82640fcc57d7516c9c1f03c41434c87c16d25
Brayann Farfán <77312455+BrayannFarfan@users.noreply.github.com>
2022-09-30 16:11:58 -0300

    prepado para la demo, con novedades y obras y info de user (#39)
    aprobado

--- app/components/progresItem/ProgresItems.js
+++ app/components/progresItem/ProgresItems.js
@@ -3,16 +3,17 @@ import { ProgressBar, Text } from "react-native-paper";
 
 
 
-export default function ProgresItem() {
+export default function ProgresItem(props) {
+
 
     return(
         <SafeAreaView style={style.cont}>
             <View  style={style.progreso}>
-                <Text style={style.titles}>Redes Cloacales </Text>
-                <Text style={style.texto}>Progreso </Text>
+                <Text style={style.titles} >{props.items.item.category.name}</Text>
+                <Text style={style.texto} >Progreso </Text>
                 <View style={style.bar}>
-                    <ProgressBar progress={0.9} color='red' />
-                    <Text style={{marginLeft:'65%', marginTop:7, fontSize:12}}>80%</Text>
+                    <ProgressBar progress={parseInt(props.items.item.value)/100 } color='red' />
+                    <Text style={{marginLeft:'65%', marginTop:7, fontSize:12}}>{props.items.item.value}%</Text>
                 </View>
             </View>
         </SafeAreaView>
@@ -21,12 +22,12 @@ export default function ProgresItem() {
 
 const style = StyleSheet.create({
     progreso:{
-        width:150,
+        width:'80%',
         margin:20,
         backgroundColor:'#fff'
     },
     cont:{
-        width:'100%'
+        width:'50%'
     },
     texto:{
         marginLeft: 10
