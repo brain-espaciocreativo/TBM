6c868676a8aba7084354e760423ca50e053e28fc
brayann verona <brayann.fave@gmail.com>
2022-10-17 14:52:41 -0300

    se modifico novedades y progreso, por que se rompio todo al usar COntext

--- app/context/AuthContext.js
+++ app/context/AuthContext.js
@@ -1,6 +1,5 @@
 import React , { useState,createContext, useEffect} from "react";
 import axios from 'axios';
-// import { useNavigation } from "@react-navigation/native";
 import * as RootNavigation from '../RootNavigation'
 
 export const AuthContext = createContext();
@@ -10,23 +9,21 @@ export const AuthProvider = ({children}) =>{
     
     const [ userInfo , setUserInfo] = useState();
     const [ loading , setLoading ] = useState(false);
-    // const navigation = useNavigation()
 
     const login = ( email , password) =>{
+        setLoading(true)
         axios.post('http://10.0.2.2:3000/auth/login',{
             email, password
         }).then( res =>{
-            let userInfo = res.data.data[0];
-            console.log('este es el user desde context',userInfo);
+            let userInfo = res.data.data;
             setUserInfo(userInfo);
-            if(userInfo === null){
-                console.log('datos incorrectos');
-            }else{
-                // navigation.navigate('Home')
-                RootNavigation.navigate('Home')
-            }
+            setTimeout(() => {
+            setLoading(false)
+            RootNavigation.navigate('Home')
+            }, 3000);
         }).catch( e =>{
             console.log(e);
+            setLoading(false)
         })
     }
     
@@ -36,7 +33,8 @@ export const AuthProvider = ({children}) =>{
         <AuthContext.Provider
          value={{
             userInfo,
-            login
+            login,
+            loading
          }}
          >
          {children}
