6f9066c1cd1cc094c3e1d234e9f7adfda16d0fbb
YoelFerreyra <89047058+YoelFerreyra@users.noreply.github.com>
2022-10-21 11:54:40 -0300

    Se agregaron validaciones para la subida de archivos

--- web/src/components/uploadFiles/UploadFiles.jsx
+++ web/src/components/uploadFiles/UploadFiles.jsx
@@ -8,7 +8,7 @@ export default function UploadFiles() {
     <>
         <form action="http://localhost:3000/upload-files" enctype="multipart/form-data" method="post">
             <input type="file" name="imagen" accept="video/mp4"></input>
-            <input type="submit" value="Buscar archivo" />
+            <input type="submit" value="Subir archivo" />
         </form>
     </>
   )
