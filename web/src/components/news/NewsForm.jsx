d967b354eb2408649323e7c78e45492253b2c95c
Eric Yoel Ferreyra <89047058+YoelFerreyra@users.noreply.github.com>
2022-11-09 19:12:40 -0300

    Guardo los cambios (#66)

--- web/src/components/news/NewsForm.jsx
+++ web/src/components/news/NewsForm.jsx
@@ -1,4 +1,4 @@
-import { Grid, TextareaAutosize, TextField, Typography, useTheme,useMediaQuery, Button } from "@mui/material";
+import { Grid, TextareaAutosize, TextField, Typography, useTheme,useMediaQuery, Button, Select, MenuItem } from "@mui/material";
 import { Box } from "@mui/system";
 import NavDashboard2 from "../navDachboard2/NavDashboard2";
 import NavDashboard from "../navDashboard/NavDashboard";
@@ -9,24 +9,30 @@ import { createOneNews } from "../../redux/slices/newSlice";
 import Swal from "sweetalert2";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-
-
+import { getAllWorks } from "../../redux/slices/workSlice";
 
 export default function NewsForm () {
 
     const dispatch = useDispatch();
     const news = useSelector(state => state.categories.news);
-
+    const works = useSelector(state => state.works.workList)
     const navigate = useNavigate();
 
     const theme = useTheme();
     const isMatch = useMediaQuery(theme.breakpoints.down('md'));
 
+    const [ selectWork, setSelectWork ] = useState("");
+    
     const [ data , setData ] = useState({
         name:'',
         description:'',
-        video:''
+        video:'',
+        workId: selectWork
     })
+
+    useEffect(()=>{
+        dispatch(getAllWorks);
+    },[dispatch]);
 
 
     const createNews = async (e) => {
@@ -47,7 +53,12 @@ export default function NewsForm () {
         setData(state => ({...state, [name]: value}));
     }
 
-    const query =  `http://localhost:3000/news?name=${data.name}&description=${data.description}`
+    const handleselectWork = (e)=>{
+        setSelectWork(e.target.value);
+        console.log('Esto es work',selectWork);
+    };
+
+    const query =  `http://localhost:3000/news?name=${data.name}&description=${data.description}&workId=${selectWork}`
     return (
         <>
             <NavDashboard />
@@ -79,8 +90,28 @@ export default function NewsForm () {
                         onChange={handleChange}
                         />
                         <input type="file" name="video" accept="video/mp4"></input>
-                        {/* <UploadFiles/> */}
-                            <input type="submit" value='enviar'/>
+                        <Typography >
+                            Selecciona una Obra
+                        </Typography>
+                        <Select
+                        labelId="demo-simple-select-autowidth-label"
+                        id="demo-simple-select-autowidth"
+                        name="name"
+                        value={selectWork}
+                        onChange={handleselectWork}
+                        label="Obras"
+                        sx={{border:'none', 
+                        boxShadow: '5px 5px 13px 2px rgba(0,0,0,0.39)'}} 
+                      >
+                      {
+                        works && works.length ? 
+                          works.map((e)=>{
+                            return <MenuItem key={e.id} value={e.name}>{e.name}</MenuItem>
+                          }) : <MenuItem value='No hay Obras'>No hay Obras</MenuItem>
+                      }
+                      </Select>
+                      <br />
+                        <input type="submit" value='enviar'/>
                     </form>
                   </Grid> 
                 </Grid>
