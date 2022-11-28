fa384748af6d53b881f381f08071a42725a4fb13
Gaston Zanuttini <gastonzanuttini@gmail.com>
2022-11-28 18:47:08 -0300

    fix build issue and add preview script

--- server/config/db.js
+++ server/config/db.js
@@ -5,7 +5,7 @@ const { Sequelize } = require("sequelize");
 
 const database = process.env.DB || 'tbm';
 const username = process.env.USERDB || 'postgres';
-const password = process.env.PASSDB || 'Yoel1234';
+const password = process.env.PASSDB || 'postgres';
 const host = process.env.HOST || 'localhost';
 
 
@@ -15,6 +15,6 @@ const sequelize = new Sequelize(database, username, password, {
   dialect: 'postgres'
 });
 
-   module.exports = {
-     conn: sequelize,
-   };
+module.exports = {
+  conn: sequelize,
+};
