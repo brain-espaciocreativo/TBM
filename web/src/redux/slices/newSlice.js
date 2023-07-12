c5ac2a416d4215e94c1b1a000510d84bed46b6d8
alee <ale.bento23@gmail.com>
2023-07-12 19:56:30 -0300

    se corrige en redux la function deleteNews para que asigne al state la nueva lista al eliminar

--- web/src/redux/slices/newSlice.js
+++ web/src/redux/slices/newSlice.js
@@ -1,55 +1,56 @@
-import { createSlice } from '@reduxjs/toolkit';
-import axios from 'axios';
+import { createSlice } from '@reduxjs/toolkit'
+import axios from 'axios'
 
 export const newSlide = createSlice({
-    name:'news',
-    initialState:{
-        newList:[]
+    name: 'news',
+    initialState: {
+        newList: [],
     },
-    reducers:{
+    reducers: {
         setNewList: (state, action) => {
-            state.newList = action.payload;
-        },
-        createNews: (state, action) =>{
             state.newList = action.payload
         },
-        deleteNews: (state, action)=>{
-            state.newList
-        }
-    }
+        createNews: (state, action) => {
+            state.newList = action.payload
+        },
+        deleteNews: (state, action) => {
+            state.newList = action.payload
+        },
+    },
 })
 
-export const { setNewList, createNews, deleteNews } = newSlide.actions;
+export const { setNewList, createNews, deleteNews } = newSlide.actions
 
-export default newSlide.reducer;
+export default newSlide.reducer
 
 export const getAllNews = () => {
     return async (dispatch) => {
-        await axios(import.meta.env.VITE_URL+'/news')
-        .then((res) => {
-            dispatch(setNewList(res.data.data));
-        })
-        .catch((error) => console.log(error))
+        await axios(import.meta.env.VITE_URL + '/news')
+            .then((res) => {
+                dispatch(setNewList(res.data.data))
+            })
+            .catch((error) => console.log(error))
     }
 }
 
 export const createOneNews = (payload) => {
-    console.log(payload);
     return async (dispatch) => {
-        await axios.post(import.meta.env.VITE_URL+'/news', payload)
-        .then((res) => {
-            dispatch(createNews(payload))
-        })
-        .catch(error => console.log(error));
+        await axios
+            .post(import.meta.env.VITE_URL + '/news', payload)
+            .then((res) => {
+                dispatch(createNews(payload))
+            })
+            .catch((error) => console.log(error))
     }
 }
 
 export const deleteOneNews = (payload) => {
     return async (dispatch) => {
-        await axios.delete(import.meta.env.VITE_URL+'/news/'+payload)
-        .then((res) => {
-            dispatch(deleteNews(payload))
-        })
-        .catch(error => console.log(error));
+        await axios
+            .delete(import.meta.env.VITE_URL + '/news/' + payload)
+            .then((res) => {
+                dispatch(deleteNews(res.data.data))
+            })
+            .catch((error) => console.log(error))
     }
 }
